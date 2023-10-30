4981253a2d2486fb33b807f1fe76e74be73f7c7a
Delight Sheriff <delightsheriff@gmail.com>
2023-10-30 13:05:39 +0100

    3. Status of your API-1

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""AirBnB Clone API"""
+
+# Import Flask and storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """Close the storage session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found_404(error):
+    """Handle non existing pages"""
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port)
