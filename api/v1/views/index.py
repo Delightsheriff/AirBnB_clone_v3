94d28ffec560bc3dde5944fe737787a386e3296f
Delight Sheriff <delightsheriff@gmail.com>
2023-10-30 13:23:13 +0100

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """API index"""
 # Import app_views from api.v1.views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -13,3 +20,26 @@ def status():
         "status": "OK"
     }
     return jsonify(temp)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """retrieves the number of each objects by type"""
+
+    avail_mod = {
+        "User": "users",
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+    }
+    model_objs = [Amenity, City, Place, Review, State, User]
+
+    count = {}
+    i = -1
+    for temp in avail_mod.keys():
+        i += 1
+        count[avail_mod[temp]] = storage.count(model_objs[i])
+
+    return jsonify(count)
