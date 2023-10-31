50b43cb7240581bc38c2602f306f987d8feb919e
Delight Sheriff <delightsheriff@gmail.com>
2023-10-31 18:28:19 +0100

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,8 @@ from models import storage
 from os import getenv
 
 app = Flask(__name__)
+CORS = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
